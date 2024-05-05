6b03d5478d2c92d0607f51a5ee68dbf47bc033dc
杨伟杰 <sia-fl@outlook.com>
2024-05-05 10:28:23 +0800

    feat: 为新模式声明调用类型

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,3 +1,7 @@
+// noinspection JSUnusedGlobalSymbols
+
+import type { ZodType, input, output } from 'zod'
+
 export interface ImportedObject {
   type: 'default' | 'named' | 'namespace'
   identifier: string
@@ -39,3 +43,33 @@ export interface JsrpcPluginOptions {
    */
   vitePort?: number
 }
+
+export declare type ZodTypeAny = ZodType<any, any, any>
+export declare type Ctx = any
+type EnsurePromise<T> = T extends PromiseLike<unknown> ? T : Promise<T>
+type Await<T> = T extends PromiseLike<infer U> ? U : T
+interface ResultWithContext<Result = unknown, Context = unknown> {
+  __blitz: true
+  value: Result
+  ctx: Context
+}
+type PipeFn<Prev, Next, PrevCtx, NextCtx = PrevCtx> = (i: Await<Prev>, c: PrevCtx) => Next extends ResultWithContext ? never : Next | ResultWithContext<Next, NextCtx>
+
+/**
+ * use case
+ *
+ * const a = pipe(zodResolver(z.object({
+ *   name: z.string(),
+ *   age: z.number(),
+ * })), (i) => {
+ *   console.log(i.name, i.age)
+ *   return 'any'
+ * })
+ *
+ * a({ name: '', age: 18 }, {})
+ *
+ * @param ab
+ * @param bc
+ */
+declare function pipe<A, B, C, CA = Ctx, CB = CA, CC = CB>(ab: PipeFn<A, B, CA, CB>, bc: PipeFn<B, C, CB, CC>): (input: A, ctx: CA) => EnsurePromise<C>
+declare function zod<Schema extends ZodTypeAny, InputType = input<Schema>, OutputType = output<Schema>>(schema: Schema): (input: InputType) => Promise<OutputType>
